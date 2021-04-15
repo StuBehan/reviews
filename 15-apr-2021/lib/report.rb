436fed592b84129ebf63a4c988a57a62b30e64b2
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-04-15 16:25:14 +0100

    added passing code for second test

--- 15-apr-2021/lib/report.rb
+++ 15-apr-2021/lib/report.rb
@@ -1,11 +1,11 @@
 class Report 
 
   def generate_report(string)
-    split_array = string.split(',')
+    split_array = string.downcase.split(',')
     green_count = 0
 
     split_array.each do |word|
-      if word == 'Green'
+      if word == 'green'
         green_count += 1
       end
     end
