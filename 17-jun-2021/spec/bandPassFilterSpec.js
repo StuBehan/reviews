c060f9dc09c4cf5e8e0eaf852cd52d432f326803
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-06-17 11:23:56 +0100

    passed test for highpass filtering

--- 17-jun-2021/spec/bandPassFilterSpec.js
+++ 17-jun-2021/spec/bandPassFilterSpec.js
@@ -3,7 +3,11 @@ describe('bandPassFilter', () => {
     expect(bandPassFilter([500])).toEqual([500])
   })
 
-  it('takes a freguency outside of the default bands and returns adjusted fq', () => {
+  it('takes a freguency outside of the default lowpass, returns adjusted fq', () => {
     expect(bandPassFilter([30])).toEqual([40])
+  })
+
+  it('takes a freq outside of the default highpass, returns adjusted fq', () => {
+    expect(bandPassFilter([1050])).toEqual([1000])
   })
 })
--- 17-jun-2021/src/bandPassFilter.js
+++ 17-jun-2021/src/bandPassFilter.js
@@ -1,10 +1,13 @@
 const bandPassFilter = (soundwave) => {
   let lowPass = 40
+  let highPass = 1000
   let adjustedSoundwave = []
 
   soundwave.forEach(freq => {
     if (freq < lowPass) {
       adjustedSoundwave.push(lowPass)
+    } else if (freq > highPass) {
+      adjustedSoundwave.push(highPass)
     } else {
       adjustedSoundwave.push(freq)
     }
